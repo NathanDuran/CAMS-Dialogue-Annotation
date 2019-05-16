345af3ce06bf189b4e4bebf5a70a4a39ddf0fdf4
Nathan <nathan_duran@hotmail.com>
2019-05-16 17:08:44 +0100

    Update Utterance View Build Functions

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from flask import Flask, url_for, render_template
+import file_utilities as utils
+from flask import Flask, url_for, render_template, jsonify
+
+json_data_path = "static/data/"
+label_data_path = "static/labels"
 
 app = Flask(__name__)
 
@@ -27,9 +31,28 @@ def practice_page():
 def annotate_page():
     return render_template('annotate.html')
 
+
+@app.route('/get_dialogue/<filename>')
+def get_dialogue(filename):
+
+    json_data = utils.load_json_data(json_data_path, filename)
+    return jsonify(json_data)
+
+
+@app.route('/get_labels/<filename>')
+def get_labels(filename):
+
+    # Load the specified labels file
+    labels_data = utils.load_txt_data(label_data_path, filename)
+    # Split into groups
+    labels_groups = utils.split_label_groups(labels_data)
+    return jsonify(labels_groups)
+
+
 @app.context_processor
 def example():
     return dict(myexample='This is an example')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
